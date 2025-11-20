98b572c83eb419e21af7ed2ea95a5a3d680a95e5
snamazova <snamazova@virt-3326.kl.dfki.de>
2025-11-20 21:42:47 +0100

    add masking script for centaur

--- rl/predictive_rl_centaur_masking.py
+++ rl/predictive_rl_centaur_masking.py
@@ -10,7 +10,7 @@ import os
 
 DATA_IN_TEST = 'data/in/test_data.csv'
 
-MODEL = 'centaur-8B'
+MODEL = 'centaur-70B'
 DATA_FOLDER_OUT = f'data/out/predictive_masking_with_prompt/{MODEL}/singles'
 
 def generate_seeds(num_seeds=20, seed=42):
@@ -274,8 +274,9 @@ def main():
 
         # Run simulation with model and tokenizer passed
         model_data = timeline[timeline['model_id'] == model_id]
-        result = simulate_participant(model_data, model, tokenizer, pipe, letter_token_ids)
-        result.to_csv(out_path, index=False)
+        result_df,masking_df = simulate_participant(model_data, model, tokenizer, pipe, letter_token_ids,save_masking=True, model_id=model_id)
+        result_df.to_csv(out_path, index=False)
+        masking_df.to_csv(f'{DATA_FOLDER_OUT}/masking_model_' + str(model_id) + '.csv', index=False)
 
 
 if __name__ == "__main__":
